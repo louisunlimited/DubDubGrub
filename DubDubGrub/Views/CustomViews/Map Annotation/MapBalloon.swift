22ec1d7ef23d3e0d8701c7fc8f1ae9d3020f836f
Louis Qian <yuq4@illinois.edu>
2022-02-24 18:16:11 -0600

    feat(Map View): Custom Map Annotation

---
+++ DubDubGrub/Views/CustomViews/Map Annotation/MapBalloon.swift
@@ -0,0 +1,28 @@
+//
+//  MapBalloon.swift
+//  DubDubGrub
+//
+//  Created by Louis Qian on 2/24/22.
+//
+
+import SwiftUI
+
+struct MapBalloon: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.minY), control: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.maxY), control: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        return path
+    }
+    
+}
+
+struct MapBalloon_Previews: PreviewProvider {
+    static var previews: some View {
+        MapBalloon()
+//            .frame(width: 300, height: 300)
+//            .border(Color.black)
+    }
+}
