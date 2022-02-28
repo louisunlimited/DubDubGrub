3c24441aff59b3e0567f4223a9b7a075118ab742
Louis Qian <yuq4@illinois.edu>
2022-02-27 21:16:06 -0600

    feat(Map View): ♿️accessibility - Voice Over

--- DubDubGrub/Views/CustomViews/LogoView.swift
+++ DubDubGrub/Views/CustomViews/LogoView.swift
@@ -12,7 +12,8 @@ struct LogoView: View {
     var frameWidth: CGFloat
     
     var body: some View {
-        Image("ddg-map-logo")
+        // Initialize with decorative if we don't want voice over
+        Image(decorative: "ddg-map-logo")
             .resizable()
             .scaledToFit()
             .frame(width: frameWidth)
--- DubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
+++ DubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
@@ -20,6 +20,7 @@ struct LocationMapView: View {
                 MapAnnotation(coordinate: location.location.coordinate, anchorPoint: CGPoint(x: 0.5, y: 0.75)) {
                     DDGAnnotation(location: location
                                   , number: viewModel.checkedInProfiles[location.id, default: 0])
+                        .accessibilityLabel(Text("Map Pin \(location.name) \(viewModel.checkedInProfiles[location.id, default: 0]) person checked in"))
                         .onTapGesture {
                             locationManager.selectedLocation = location
                             
@@ -35,6 +36,7 @@ struct LocationMapView: View {
             VStack {
                 LogoView(frameWidth: 125)
                     .shadow(radius: 10)
+//                    .accessibilityHidden(true)
                 
                 Spacer()
             }
